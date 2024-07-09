4c4b5b4d68ac98e42c3848307704d9eba4abe8f6
Iyan Theekshana <iyan.dev123@gmail.com>
2024-07-09 20:48:36 +0200

    added responsiveness to navbar

---
+++ main.js
@@ -0,0 +1,11 @@
+const hamContainer = document.querySelector("#header__main-nav");
+
+const hamburger = document.querySelector(".header__header--hamburger");
+
+const links = document.querySelectorAll(".header__main--links li");
+
+hamburger.addEventListener("click", () => {
+  hamContainer.classList.toggle("clicked");
+  hamburger.classList.toggle("changedHamColor");
+  links.forEach((link) => link.classList.toggle("fade"));
+});
